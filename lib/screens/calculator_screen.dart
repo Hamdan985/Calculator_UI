268edfefe00dd5ed082a64e0ddc03439e1248219
Hamdan985 <hamdan246@gmail.com>
2020-06-27 20:23:02 +0530

    initial commit

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -10,7 +10,20 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
-      
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.deepPurple,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
